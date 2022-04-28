fd024e645218a8b3f95592bacf231fb3012c2528
Emanuele D'Agata <ema.dagata@gmail.com>
2022-04-28 21:38:55 +0200

    20220428

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
